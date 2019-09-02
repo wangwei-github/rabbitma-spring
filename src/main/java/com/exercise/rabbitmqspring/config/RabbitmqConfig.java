168e2ed7071c230616efb3b5e014e3f02c7ca7a8
wangwei <752853055@qq.com>
2019-09-02 20:53:08 +0800

    rabbitmq与springboot整合 RabbitTemplate模板的使用

--- src/main/java/com/exercise/rabbitmqspring/config/RabbitmqConfig.java
+++ src/main/java/com/exercise/rabbitmqspring/config/RabbitmqConfig.java
@@ -1,10 +1,11 @@
 package com.exercise.rabbitmqspring.config;
 
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -24,5 +25,60 @@ public class RabbitmqConfig {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         rabbitAdmin.setAutoStartup(true);
         return rabbitAdmin;
+    }
+
+    /*针对消费者配置
+    * 设置交换机类型
+    * 将队列绑定到交换机*/
+
+    @Bean
+    public DirectExchange exchange001(){
+        return new DirectExchange("exchange001", true, false);
+    }
+    @Bean
+    public Queue queue001(){
+        return new Queue("queue001", true);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring001.*");
+    }
+
+    @Bean
+    public TopicExchange exchange002(){
+        return new TopicExchange("exchange002", true, false);
+    }
+    @Bean
+    public Queue queue002(){
+        return new Queue("queue002", true);
+    }
+
+    @Bean
+    public Binding binding2(){
+        return BindingBuilder.bind(queue002()).to(exchange002()).with("spring002.*");
+    }
+
+    @Bean
+    public Queue queue004(){
+        return new Queue("queue004", true);
+    }@Bean
+    public Queue queue005(){
+        return new Queue("queue005", true);
+    }
+
+    @Bean
+    public Binding binding3(){
+        return BindingBuilder.bind(queue004()).to(exchange002()).with("spring004.*");
+    }
+    @Bean
+    public Binding binding4(){
+        return BindingBuilder.bind(queue005()).to(exchange002()).with("spring005.*");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        return rabbitTemplate;
     }
 }
--- src/test/java/com/exercise/rabbitmqspring/configtest/ConfigTest.java
+++ src/test/java/com/exercise/rabbitmqspring/configtest/ConfigTest.java
@@ -2,6 +2,7 @@ package com.exercise.rabbitmqspring.configtest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -45,5 +46,32 @@ public class ConfigTest {
         rabbitAdmin.declareBinding(BindingBuilder.bind(new Queue("spring.fanout.queue",false,false,false)).
                 to(new FanoutExchange("spring.fanout.exchange",false,false)));
 
+    }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Test
+    public void template(){
+        //创建消息
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.getHeaders().put("001", "第一次描述");
+        messageProperties.getHeaders().put("002", "第二次描述");
+        Message message = new Message("使用messageProperties发送消息！".getBytes(), messageProperties);
+
+        //发送消息
+        //	direct路由需要完全匹配
+        //	topic可以模糊匹配
+        rabbitTemplate.convertAndSend("exchange001","spring001.*",message, message1 -> {
+            message1.getMessageProperties().getHeaders().put("额外发送消息", "pretty");
+            return message1;
+        });
+    }
+    @Test
+    public void template2(){
+
+
+        //发送消息
+        rabbitTemplate.convertAndSend("exchange002","spring002.a","交换机002");
+        rabbitTemplate.convertAndSend("exchange002","spring004.a","交换机002，队列4");
     }
 }
