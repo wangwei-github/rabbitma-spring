bfc594e62f10f77644aa08df008c70c2db2aedf9
wangwei <752853055@qq.com>
2019-09-05 20:11:19 +0800

    rabbitMq springboot整合Producer配置

---
+++ src/test/java/com/exercise/rabbitmqspring/configtest/springboottest/SpringBootTests.java
@@ -0,0 +1,40 @@
+package com.exercise.rabbitmqspring.configtest.springboottest;
+
+import com.exercise.rabbitmqspring.producer.RabbitSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    private RabbitSender rabbitSender;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("YYYY/MM/DD HH:mm:ss sss");
+
+    @Test
+    public  void testSend(){
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("num", "123456");
+        properties.put("send_time", sdf.format(new Date()));
+        rabbitSender.send("Hello SpringBoot RabbitMQ! ",properties);
+    }
+
+
+
+
+}
